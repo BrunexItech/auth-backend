dfdcfc55453d0bc862a5632c21ad32c0f32e5241
BrunexItech <brunosharif89@gmail.com>
2025-07-21 16:51:07 +0300

    Changes

--- create_superuser.py
+++ create_superuser.py
@@ -1,16 +1,1 @@
-import os
-import django
-from django.contrib.auth import get_user_model
 
-django.setup()
-User = get_user_model()
-
-username = "Brunex"
-email = "brunokahindi8053@gmail.com"
-password = "Bruno8053shari"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser created.")
-else:
-    print("Superuser already exists.")
