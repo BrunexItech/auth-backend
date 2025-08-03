9f252ae2be77ae910f297e0b1b4cf610e4fa3856
BrunexItech <brunosharif89@gmail.com>
2025-08-03 10:01:45 +0300

    cloudinary

--- backend/settings.py
+++ backend/settings.py
@@ -89,16 +89,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
-
 '''DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}'''
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
 
 
 # Password validation
